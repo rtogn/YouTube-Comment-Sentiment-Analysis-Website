5d731910f8facd17816a43b95a15e1f2a3131fe6
Hafsa Hassan <hafsafarah900@gmail.com>
2023-01-26 20:43:19 -0500

    configure api

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-import flask, os, requests
+import flask
+import os
+import requests
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+APIKEY = os.getenv("APIKEY")
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
+
     return flask.render_template(
         "index.html"
     )
 
+
 app.run(
-        use_reloader = True,
-        debug=True
+    use_reloader=True,
+    debug=True
 )
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
