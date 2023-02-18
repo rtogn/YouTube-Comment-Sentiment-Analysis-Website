3c8de4618b0d34347c9f11a6d63b11e04f6f0a85
Rob Tognoni <60898339+rtogn@users.noreply.github.com>
2023-02-17 19:30:54 -0500

    Add files via upload
    Fixed reference to importing files in admin functions and sql_requests .py

---
+++ YTSA_Core_Files/YTSA_Core_Files/sql_requests.py
@@ -0,0 +1,16 @@
+import YTSA_Core_Files.sql_models as sql_models
+
+def update_sentiment_average(target_entry, new_score):
+    # Generic function to update any runnign sentiment score average
+    # Can work for channel or per video (in top vids) scores
+    assert new_score >= 0.0 and new_score <= 1.0
+    curAvrg = target_entry.sentiment_score_average
+    entry_count = target_entry.entry_count
+    sum = (curAvrg * entry_count)
+    sum += new_score
+    entry_count += 1
+
+    # update average and count of contributing entries
+    target_entry.sentiment_score_average = sum / entry_count
+    target_entry.entry_count = entry_count
+
