e2e44fd354adabf6db146bfc689d0aa8a20fbc82
Jaz <barnett.jazmine2000@gmail.com>
2023-03-24 22:04:59 -0400

    beginning of tests. Yikes. Plus some added forms.

---
+++ functions.test.js
@@ -0,0 +1,30 @@
+const { spawnSync } = require('child_process');
+const { mock, it } = require('node:test');
+const { describe } = require('yargs');
+const LoginFunctions = require('./functions');
+
+let spy;
+beforeEach (() => {
+    spy = jest.spyOn(document, 'getElementById');
+});
+
+describe ("All login and register popup tests", () => {
+    describe('with new item', () => {
+        let mockElement;
+        beforeEach( () => {
+            mockElement = document.createElement();
+            spy.mockReturnValue(mockElement);
+        });
+    
+        it('id of pop-up box should change to display', () => {
+            spy.mockElement.LoginFunctions.openLog(mockElement);
+            expect(mockElement.style.display).toBe('none');
+        });
+                
+        it('id of register-up box should change to display', () => {
+            spy.mockElement.LoginFunctions.openReg(mockElement);
+            expect(mockElement.style.display).toBe('none');
+        });
+
+    });
+});
